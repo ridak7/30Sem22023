bce57af3decfa43a66a7d5ba4a0f7e52b545bde2
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2023-03-15 09:36:14 -0600

    9:36AM

--- Chapter11/src/GradeBook.java
+++ Chapter11/src/GradeBook.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.util.Scanner;
+
 
 public class GradeBook 
 {
@@ -32,5 +34,80 @@ public class GradeBook
 		}
 
 	}
+	
+	//Obtain student grades from a file
+	//The grades array has been populated with student grades
+	public void loadGrades(String fileName)
+	{
+		File gradeFile = new File(fileName);
+		FileReader in;
+		BufferedReader read;
+		String stuGrade;
+		
+		try
+		{
+			in = new FileReader(gradeFile);
+			read = new BufferedReader(in);
+			
+			//Read grades into the array
+			for(int stu = 0; stu < grades.length; stu++)
+			{
+				for(int grade = 0; grade < grades[0].length; grade++)
+				{
+					stuGrade = read.readLine();
+					grades[stu][grade] = Integer.parseInt(stuGrade);
+				}
+			}
+			read.close();
+			in.close();
+			
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File does not exist or could not be found. ");
+			System.err.print("FileNotFoundexception: " + e.getMessage());
+		}
+		catch(IOException e)
+		{
+			System.out.println("Problem reading file. ");
+			System.err.print("IOException: " + e.getMessage());
+		}
+		
+	}
+	
+	//Write student grades to a file
+	public void storeGrades(String fileName)
+	{
+		File gradeFile = new File(fileName);
+		FileWriter out;
+		BufferedWriter write;
+		
+		try
+		{
+			out = new FileWriter(gradeFile);
+			write = new BufferedWriter(out);
+			
+			//Write grades to a file
+			for(int stu = 0; stu < grades.length; stu++)
+			{
+				for(int grade = 0; grade < grades[0].length; grade++)
+				{
+					write.write(String.valueOf(grades[stu][grade]));
+					write.newLine();
+				}
+			}
+			write.close();
+			out.close();
+			
+			
+		}
+		catch(IOException e)
+		{
+			System.out.println("Problem reading file. ");
+			System.err.print("IOException: " + e.getMessage());
+		}
+		
+	}
+	
 
 }
