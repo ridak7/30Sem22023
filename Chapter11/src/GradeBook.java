1eb05ddba5ab291715514b2771e1071bee4d2f77
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2023-03-16 09:22:45 -0600

    9:22AM

--- Chapter11/src/GradeBook.java
+++ Chapter11/src/GradeBook.java
@@ -1,18 +1,16 @@
 import java.io.*;
 import java.util.Scanner;
 
-
 public class GradeBook 
 {
-	private int[][] grades;//Declare 2D array 
-	
+	//Declare 2D array
+	private int[][] grades;
 	
 	//Constructor method for GradeBook
 	public GradeBook(int numStudents, int numGrades)
 	{
 		//GradeBook object has been created
-		grades = new int[numStudents][numGrades];		
-		
+		grades = new int[numStudents][numGrades];
 	}
 	
 	//Obtain student grades from the user
@@ -21,7 +19,7 @@ public class GradeBook
 		Scanner input = new Scanner(System.in);
 		int stuGrade;
 		
-		for(int stu = 0; stu < grades.length; stu++)
+		for(int stu =0; stu < grades.length; stu++)
 		{
 			for(int grade = 0; grade < grades[0].length; grade++)
 			{
@@ -32,7 +30,7 @@ public class GradeBook
 				grades[stu][grade] = stuGrade;
 			}
 		}
-
+		
 	}
 	
 	//Obtain student grades from a file
@@ -50,7 +48,7 @@ public class GradeBook
 			read = new BufferedReader(in);
 			
 			//Read grades into the array
-			for(int stu = 0; stu < grades.length; stu++)
+			for(int stu =0; stu < grades.length; stu++)
 			{
 				for(int grade = 0; grade < grades[0].length; grade++)
 				{
@@ -72,7 +70,6 @@ public class GradeBook
 			System.out.println("Problem reading file. ");
 			System.err.print("IOException: " + e.getMessage());
 		}
-		
 	}
 	
 	//Write student grades to a file
@@ -86,9 +83,9 @@ public class GradeBook
 		{
 			out = new FileWriter(gradeFile);
 			write = new BufferedWriter(out);
-			
+		
 			//Write grades to a file
-			for(int stu = 0; stu < grades.length; stu++)
+			for(int stu =0; stu < grades.length; stu++)
 			{
 				for(int grade = 0; grade < grades[0].length; grade++)
 				{
@@ -98,16 +95,73 @@ public class GradeBook
 			}
 			write.close();
 			out.close();
-			
-			
 		}
 		catch(IOException e)
 		{
 			System.out.println("Problem reading file. ");
 			System.err.print("IOException: " + e.getMessage());
 		}
+	}
+	
+	//Display student grades
+	public void showGrades()
+	{
+		System.out.println("Student Gerades: ");
+		
+		//Test number labels
+		System.out.format("%5s", "");
+		
+		for(int i=0; i < grades[0].length; i++)
+		{
+			System.out.format("%5s", i + 1);
+		}
+		System.out.println();
+		
+		//Display student numbers and test scores
+		for(int stu =0; stu < grades.length; stu++)
+		{
+			System.out.format("%5s", stu + 1);
+		
+			for(int grade = 0; grade < grades[0].length; grade++)
+			{
+				System.out.format("%5s", grades[stu][grade]);
+			}
+			System.out.println();
+		}
+	}
+	
+	//Return the average for student stu
+	public double studentAvg(int stu)
+	{
+		int sum = 0;
+		double avg = 0;
+		
+		for(int grade = 0; grade < grades[0].length; grade++)
+		{
+			sum =  sum + grades[stu - 1][grade];
+		}
+		
+		avg = (double)sum/grades[0].length;
+		
+		return avg;
 		
 	}
 	
-
+	//Returns the test average for the a particular test called test
+	public double testAvg(int test)
+	{
+		int sum = 0;
+		double avg = 0;
+		
+		for(int stu =0; stu < grades.length; stu++)
+		{
+			sum = sum + grades[stu][test -1];
+		}
+		avg = (double)sum/ grades.length;
+		
+		return avg;
+		
+	}
+	
+	
 }
