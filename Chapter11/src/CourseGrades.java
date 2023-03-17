7b5dd4fc2353e96fa54b934c82ccef4163744e32
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2023-03-17 09:31:33 -0600

    9:31AM Friday

--- Chapter11/src/CourseGrades.java
+++ Chapter11/src/CourseGrades.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
-public class CourseGrades {
+public class CourseGrades 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		String choice, fileName;
+		GradeBook javaClass = new GradeBook(3, 2);
 
+		System.out.print("Do you need to Enter grades or Load grades from a file? ");
+		choice = input.nextLine();
+		choice = choice.toLowerCase();
+		if (choice.equals("e")) {
+			javaClass.getGrades();
+		} else {
+			System.out.print("Enter the name of the file containing the grades: ");
+			fileName = input.nextLine();
+			javaClass.loadGrades(fileName);
+		}
+		javaClass.showGrades();
+		System.out.println("The average for student 3 is: " + javaClass.studentAvg(3));
+		System.out.println("The average score on test 2 is: " + javaClass.testAvg(2));
+		javaClass.storeGrades("..\\Feb13ClassDemo\\src\\test2.txt");
 	}
 
 }
