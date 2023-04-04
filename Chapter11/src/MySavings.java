cd71ccf5f6eb5ac71484bcf2f1f131b4b135e72c
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2023-04-04 09:33:41 -0600

    9:33AM

---
+++ Chapter11/src/MySavings.java
@@ -0,0 +1,35 @@
+
+public class MySavings 
+{
+	//Loads the PiggyBank object from a file
+	
+	public static PiggyBank load()
+	{
+		//Create a new file
+		//Create a PiggyBank object
+		
+		//Read object example of on page 289
+	}
+	
+	
+	
+	
+	
+	//Stores the PiggyBank object to a file
+	
+	public static void store(PiggyBank money)
+	{
+		//Create a new file
+		
+		//Write object example on page 289
+	}
+		
+	
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
