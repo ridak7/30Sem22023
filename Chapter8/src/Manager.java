1d678e9cd7a262ade4de9ca795f2990de7292d6b
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2023-04-05 09:42:18 -0600

    9:42AM

--- Chapter8/src/Manager.java
+++ Chapter8/src/Manager.java
@@ -1,5 +1,35 @@
 
 public class Manager extends Employee 
 {
+	private double yearlySalary;
+	
+	public Manager(String fN, String lN, double salary)
+	{
+		super(fN, lN);
+		yearlySalary = salary;
+	}
+	
+	public double getYearlySalary()
+	{
+		return yearlySalary;
+	}
+	
+	public void setYearlySalary(double salary)
+	{
+		yearlySalary = salary;
+	}
+	
+	public double pay(double weeks)
+	{
+		double payEarned;
+		payEarned = (yearlySalary/52)*weeks;
+		return payEarned;
+	}
+	
+	public String toString()
+	{
+		return(super.toString() + ", manager");
+	}
+	
 	
 }
