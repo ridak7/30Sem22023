6827fb4b93254d2c8d65f44ad7f1541b463ea83c
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2023-04-11 09:33:51 -0600

    9:33AM

--- Chapter8/src/Associate.java
+++ Chapter8/src/Associate.java
@@ -14,7 +14,7 @@ public class Associate extends Employee
 		double payEarned;
 		
 		if(hours > 40)
-		{
+		{//
 			payEarned = ((hourlyPayRate * 40) + (hourlyPayRate * 1.5)*(hours - 40));
 		}
 		else
